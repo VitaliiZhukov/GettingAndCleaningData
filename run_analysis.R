cf187116b425426c91c93c3ac3d94c2645c87fc9
VitaliiZhukov <zhukov-vit@yandex.ru>
2015-04-25 19:44:26 +0300

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,9 +1,12 @@
+run_analysis<-function(){
+  
 X_train<-read.table("Dataset/train/X_train.txt")
 X_test<-read.table("Dataset/test/X_test.txt")
 Y_train<-read.table("Dataset/train/y_train.txt")
 Y_test<-read.table("Dataset/test/y_test.txt")
 subject_train<-read.table("Dataset/train/subject_train.txt")
 subject_test<-read.table("Dataset/test/subject_test.txt")
+activities<-read.table("Dataset/activity_labels.txt")
 features<-read.table("Dataset/features.txt")
 features$V2<-as.character(features$V2)
 
@@ -11,7 +14,7 @@ train<-cbind(subject_train,Y_train,X_train)
 test<-cbind(subject_test,Y_test,X_test)
 dataset<-rbind(train,test)
 ids<-features[((grepl("mean",features$V2)|grepl("std",features$V2))),1]
-ids<-ids+2 # сдвиг на 2 позиции вправо, т. к. первые два поля - это subject_id и y
+ids<-ids+2  
 dataset<-dataset[,c(1,2,ids)]
 dsNames<-features[((grepl("mean",features$V2)|grepl("std",features$V2))),2]
 colnames(dataset)<-as.factor(c("subjectID","activityID",dsNames))
@@ -31,5 +34,7 @@ for (i in 1:30){
 colnames(newDS)<-colnames(dataset)
 write.table(newDS,file="newDS.txt",row.name=FALSE)
 
-activities<-read.table("Dataset/activity_labels.txt")
-dataset$activity_labels<-activities[dataset$activityID,2]
+neDS$activity_labels<-activities[newDS$activityID,2]
+
+return(newDS)
+}
